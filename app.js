38b5f6694b341e53f2b4870b46d6e2de6bd3c973
bhuvan malik <bhuvanmalik007@gmail.com>
2016-06-07 01:08:23 +0530

    first

---
+++ app.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+var app = express();
+
+var character,quote;
+var json = [];
+//character : "", quote : ""
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function(req,res){
+  res.send('QUOTE-UNQUOTE');
+});
+
+
+app.get('/scrape', function(req, res){
+  // The URL we will scrape from - in our example Anchorman 2.
+
+  url = 'http://www.imdb.com/title/tt0944947/quotes';
+
+  // The structure of our request call
+  // The first parameter is our URL
+  // The callback function takes 3 parameters, an error, response status code and the html
+
+  request(url, function(error, response, html){
+
+    // First we'll check to make sure no errors occurred when making the request
+
+    if(!error){
+      // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+      var $ = cheerio.load(html);
+
+      //console.log($);
+
+      // Finally, we'll define the variables we're going to capture
+
+      //var title, release, rating;
+      //var json = { title : "", release : "", rating : ""};
+
+      $('.sodatext').each(function(i,element){
+
+        // Let's store the data we filter into a variable so we can easily see what's going on.
+
+        var data = $(this);
+
+        json.push({});
+
+        var temp = data.children().text();
+
+        function replaceAll(str, find, replace) {
+          return str.replace(new RegExp(find, 'g'), replace);
+        }
+
+        temp=replaceAll(temp,'\n','');
+
+        json[i].character = data.children().children().children('.character').text();
+        json[i].quote = temp;
+      });
+
+      res.json(json);
+
+    }
+  })
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bhuvanmalik on 14/12/15.
+ */
+
+var gulp = require('gulp'),
+    nodemon = require('nodemon');
+
+gulp.task('default', function(){
+    nodemon({
+
+        "scripts": {
+            "start": "nodemon"
+        },
+        ext: 'js',
+
+        ignore: ['./node_modules/**']
+    }).on('restart', function(){
+        console.log('restarting..');
+    });
+});
+
